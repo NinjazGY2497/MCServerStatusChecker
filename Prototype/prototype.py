ba23c47d1255e51ffc7a74b5538596e701f31445
NinjazGY2497 <gyagau@gmail.com>
2026-04-24 17:59:55 -0400

    corrected typo in error logging

--- Prototype/prototype.py
+++ Prototype/prototype.py
@@ -24,7 +24,7 @@ def serverStatus(server: JavaServer, currentRecord: Record):
             if attempt < 4:
                 time.sleep(2)
             else:
-                log(f"({attempt + 1}) [{currentRecord.attempts}] Aborting status check for this minute because ran out of attempts. {type(e).__name__}: {e}.")
+                log(f"({attempt + 1}) [{currentRecord.time}] Aborting status check for this minute because ran out of attempts. {type(e).__name__}: {e}.")
                 currentRecord.error = f"{type(e).__name__}: {e}"[:200]
                 return None
 
